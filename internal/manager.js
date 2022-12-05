ceffb4712b1ba6703cacfd7ef0313bb640389508
Paige Barrow <69479358+PageNotFound40@users.noreply.github.com>
2022-12-05 15:34:58 -0600

    d

--- internal/manager.js
+++ internal/manager.js
@@ -1,8 +1,8 @@
 const internal_pages = {
     // YOU MUST ADD YOUR INTERNAL PAGE TO THIS FILE IF YOU WANT IT TO WORK!
     // This index is how HT can reference what page to what file
-    newtab: "/internal/newTab/main.html",
-    extensionsmarketplace: "/internal/extensions/marketplace.html",
-    extensions: "/internal/extensions/index.html",
-    games: "/internal/g/index.html"
+    newtab: "https://paigeunb.github.io/internal/newTab/main.html",
+    extensionsmarketplace: "https://paigeunb.github.io/internal/extensions/marketplace.html",
+    extensions: "https://paigeunb.github.io/internal/extensions/index.html",
+    games: "https://paigeunb.github.io/internal/g/index.html"
 }
